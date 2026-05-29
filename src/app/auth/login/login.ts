d747bd04ab04aceaabcdc6f2b7693129511f9bd7
Guillermo Velasquez <velasquezperezjg@gmail.com>
2026-05-29 13:46:36 -0500

    feat(login): add form validators

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {}
+export class Login {
+  loginForm = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6)
+    ])
+  })
+
+  onSubmit() {
+    // TODO: Evaluate all validators in form
+    // TODO: Implement authtentication service to api rest
+    console.log(this.loginForm.value);
+  }
+
+  isFieldInvalid(fieldName: string) {
+    const field = this.loginForm.get(fieldName);
+    return !!(field?.invalid && field?.touched);
+  }
+}
