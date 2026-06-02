db5a9908f08cf425ad70c057379bcea8efdbd8d3
Guillermo Velasquez <velasquezperezjg@gmail.com>
2026-06-01 23:23:57 -0500

    feat(login): add button state management

--- src/app/auth/login/login.ts
+++ src/app/auth/login/login.ts
@@ -1,27 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+type ButtonState = 'idle' | 'loading' | 'disabled';
 
 @Component({
   selector: 'app-login',
   imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
 })
 export class Login {
   loginForm = new FormGroup({
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email
-    ]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(6)
-    ])
-  })
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
-  onSubmit() {
-    // TODO: Evaluate all validators in form
-    // TODO: Implement authtentication service to api rest
+  private readonly formStatus = toSignal(this.loginForm.statusChanges, {
+    initialValue: this.loginForm.status,
+  });
+
+  private readonly _buttonState = signal<ButtonState>('idle');
+
+  readonly buttonState = computed<ButtonState>(() => {
+    if (this.formStatus() === 'INVALID') return 'disabled';
+    return this._buttonState();
+  });
+
+  readonly buttonIsDisabled = computed<boolean>(() => {
+    if (this._buttonState() === 'disabled' || this._buttonState() === 'loading') return true;
+    return false;
+  });
+
+  readonly buttonClasses = computed(() => {
+    switch (this.buttonState()) {
+      case 'loading':
+        return 'bg-primary text-white opacity-75 cursor-progress';
+      case 'disabled':
+        return 'bg-primary text-white opacity-50 cursor-not-allowed';
+      default:
+        return 'bg-primary text-white opacity-100 cursor-pointer';
+    }
+  });
+
+  async onSubmit() {
+    if (this.loginForm.invalid) return;
+
+    this._buttonState.set('loading');
+
+    try {
+      // TODO: Implement authtentication service to api rest
+      await new Promise((r) => setTimeout(r, 2000));
+    } finally {
+      this._buttonState.set('idle');
+    }
     console.log(this.loginForm.value);
   }
 
