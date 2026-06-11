0bc571745f4768f0c4dd186c51b772bb0354374c
Guillermo Velásquez <102449689+guillermovez@users.noreply.github.com>
2026-06-11 03:17:59 -0500

    feat(sidebar): create sidebar component #2
    * feat: add layout and sidebar components
    
    * feat(sidebar): complete static sidebar
    
    * feat(sidebar): add variable access to data structure
    
    * refactor: change naming
    
    * refactor: delete unused imports

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { Login } from './auth/login/login';
+import { Layout } from './layout/layout';
 
 export const routes: Routes = [
   {
     path: '',
     component: Login,
     title: 'Inicio de sesión'
+  },
+  {
+    path: 'app',
+    component: Layout
   }
 ];
