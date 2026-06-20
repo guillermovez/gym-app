18652715b6f86bdda9a2a50bb6428fdf826d246b
Guillermo Velásquez <102449689+guillermovez@users.noreply.github.com>
2026-06-19 19:36:17 -0500

    feat(member): add member component (#5)
    * feat: create section for client
    
    * feature:finalcorrection customer or member section
    
    * refactor: rename client component to member
    
    ---------
    
    Co-authored-by: Bruno <brunosalinasfarias@gmail.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Member } from './client/member';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Member],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
